75945c756959b110ea13db33fb2c7395179744b0
Ambarish Singh <105585526+Ambarish-224@users.noreply.github.com>
2023-03-05 08:31:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")  #localhost: Port_number?home
+
+
+def home():
+    return "This is our first Flask Application"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
