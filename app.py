f94c0528d40f064d04d9b39292ba578e0104f90e
Ambarish_224 <ambarish.singh224@gmal.com>
2023-03-05 20:36:47 +0530

    folder update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods = ['GET')  #localhost: Port_number?home
+@app.route('/')  #localhost: Port_number?home
 
 
 def home():
@@ -12,4 +12,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host ='0.0.0.0', port=8000)
